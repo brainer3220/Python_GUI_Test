b81b825f9cf40e28647bddf515e9e3c3a20e954d
brainer3220 <40418544+brainer3220@users.noreply.github.com>
2018-12-14 00:47:58 +0900

    배경추가

--- Game.py
+++ Game.py
@@ -9,6 +9,9 @@ screen = pygame.display.set_mode((width, height))
 
 # 이미지를 갖고온다.
 player = pygame.image.load("Shoot_Game/resources/images/dude.png")
+grass = pygame.image.load("Shoot_Game/resources/images/grass.png")
+castle = pygame.image.load("Shoot_Game/resources/images/castle.png")
+
 
 # 계속 화면이 보이도록 한다.
 while True:
@@ -16,6 +19,9 @@ while True:
     screen.fill((0,0,0))    # (R, G, B)
     
     # 모든 요소들을 다시 그린다.
+    for x in range(width//grass.get_width()+1):
+        for y in range(height//grass.get_height()+1):
+            screen.blit(grass, (x*100,y*100))
     screen.blit(player, (100,100))
 
     # 화면을 다시 그린다.

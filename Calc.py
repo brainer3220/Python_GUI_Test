046e72d047e563267bba3d8c6e55979dd99172db
brainer3220 <40418544+brainer3220@users.noreply.github.com>
2018-12-13 20:25:11 +0900

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -22,3 +22,23 @@ from tkinter import scrolldtext
 
 multi_text = srolledtext.ScrolldeText(window, width=30, height=5, wrap=tk,WORD)
 multi.text.pack()
+
+
+display = tk.Entry(window).grid(columnspan=5, row=0)
+tk.Button(window, text='7').grid(column=0, row=1)
+tk.Button(window, text='8').grid(column=0, row=1)
+tk.Button(window, text='9').grid(column=0, row=1)
+tk.Button(window, text='C').grid(column=0, row=1)
+tk.Button(window, text='4').grid(column=0, row=1)
+tk.Button(window, text='5').grid(column=0, row=1)
+tk.Button(window, text='6').grid(column=0, row=1)
+tk.Button(window, text='x').grid(column=0, row=1)
+tk.Button(window, text='/').grid(column=0, row=1)
+tk.Button(window, text='1').grid(column=0, row=1)
+tk.Button(window, text='2').grid(column=0, row=1)
+tk.Button(window, text='3').grid(column=0, row=1)
+tk.Button(window, text='+').grid(column=0, row=1)
+tk.Button(window, text='-').grid(column=0, row=1)
+tk.Button(window, text='0').grid(column=0, row=1)
+tk.Button(window, text='.').grid(column=0, row=1)
+tk.Button(window, text='=').grid(column=0, row=1)

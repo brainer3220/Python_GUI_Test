9e673ae4da29fd03f1c4beb4653d5589a37a1eee
brainer3220 <40418544+brainer3220@users.noreply.github.com>
2018-12-13 20:21:12 +0900

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -7,3 +7,18 @@ window.title('Calculator;')
 
 hello_world = tk.Label(window, text='Hello, World!')
 hello_world.pack()
+
+button1 = tk.Button(winodw, text='Button')
+button1.pack()
+
+input = tk.Entry(window, width=10)
+input.pack()
+
+from thkinter import ttk
+combolist = ttk.Combobox(window, width=20, state'readonly')
+combolist.pack()
+
+from tkinter import scrolldtext
+
+multi_text = srolledtext.ScrolldeText(window, width=30, height=5, wrap=tk,WORD)
+multi.text.pack()

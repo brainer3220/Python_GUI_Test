b424106924a0505efe3f9471ace0787fec131736
brainer3220 <40418544+brainer3220@users.noreply.github.com>
2018-12-14 00:33:37 +0900

    Update and rename Calc.py to Game.py

---
+++ Game.py
@@ -0,0 +1,28 @@
+# 파이게임 모듈을 불러 온다.
+import pygame 
+
+# 초기화 시킨다.
+pygame.init()
+
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+
+# 이미지를 갖고온다.
+player = pygame.image.load("Shoot_Game/resources/images/dude.png")
+
+# 계속 화면이 보이도록 한다.
+while True:
+    # 화면을 깨끗하게 한다.
+    screen.fill((0,0,0))    # (R, G, B)
+    
+    # 모든 요소들을 다시 그린다.
+    screen.blit(player, (100,100))
+
+    # 화면을 다시 그린다.
+    pygame.display.flip()
+
+    # 게임을 종료한다.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)

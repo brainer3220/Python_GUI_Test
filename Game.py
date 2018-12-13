e5863999c41670b2af5a0d6abd3e2d17bf155e90
brainer3220 <40418544+brainer3220@users.noreply.github.com>
2018-12-14 00:51:41 +0900

    Castle 추가

--- Game.py
+++ Game.py
@@ -22,6 +22,11 @@ while True:
     for x in range(width//grass.get_width()+1):
         for y in range(height//grass.get_height()+1):
             screen.blit(grass, (x*100,y*100))
+    
+    screen.blit(castle, (0,30))
+    screen.blit(castle, (0,135))
+    screen.blit(castle, (0,240))
+    screen.blit(castle, (0,345))
     screen.blit(player, (100,100))
 
     # 화면을 다시 그린다.
